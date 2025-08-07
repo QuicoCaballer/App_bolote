41cb8247a994f577cd3249aa4e462a4ffd32ef46
QuicoCaballer <quicocabaguti@gmail.com>
2025-08-07 15:23:21 +0100

    Code_final.py

--- Code_final.py
+++ Code_final.py
@@ -29,9 +29,9 @@ with col1:
 
     # Botones rápidos solo afectan a Vos
     st.markdown("**Puntuaciones rápidas**")
-    if st.button("Tercera (1 punto)", key="tercera_vos"):
-        st.session_state.rapido_vos += 1
-    if st.button("Bolote_Rebolote (20)", key="bolote_vos"):
+    if st.button("Tercera", key="tercera_vos"):
+        st.session_state.rapido_vos += 20
+    if st.button("Bolote_Rebolote", key="bolote_vos"):
         st.session_state.rapido_vos += 20
     for pts in [50, 100, 150, 200]:
         if st.button(f"{pts} puntos", key=f"rapido_{pts}_vos"):
@@ -48,9 +48,9 @@ with col2:
 
     # Botones rápidos solo afectan a Nos
     st.markdown("**Puntuaciones rápidas**")
-    if st.button("Tercera (1 punto)", key="tercera_nos"):
-        st.session_state.rapido_nos += 1
-    if st.button("Bolote_Rebolote (20)", key="bolote_nos"):
+    if st.button("Tercera", key="tercera_nos"):
+        st.session_state.rapido_nos += 20
+    if st.button("Bolote_Rebolote", key="bolote_nos"):
         st.session_state.rapido_nos += 20
     for pts in [50, 100, 150, 200]:
         if st.button(f"{pts} puntos", key=f"rapido_{pts}_nos"):
