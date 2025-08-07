728a325925d6a6a05335d310a67def572fa519c9
QuicoCaballer <quicocabaguti@gmail.com>
2025-08-07 16:22:23 +0100

    Code_final.py

--- Code_final.py
+++ Code_final.py
@@ -15,20 +15,19 @@ for key in [
         else:
             st.session_state[key] = 0
 
-st.title("Marcador Truco - Vos / Nos")
+st.title("BOLOTE")
 
 # Columnas para vos y nos
 col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Vos")
-    input_vos = st.number_input("Puntos ronda (Vos)", min_value=0, max_value=162, key="input_vos_col")
+    input_vos = st.number_input( min_value=0, max_value=162, key="input_vos_col")
     if input_vos:
         st.session_state.input_vos = input_vos
         st.session_state.input_nos = 162 - input_vos
 
     # Botones rápidos solo afectan a Vos
-    st.markdown("**Puntuaciones rápidas**")
     if st.button("Tercera", key="tercera_vos"):
         st.session_state.rapido_vos += 20
     if st.button("Bolote_Rebolote", key="bolote_vos"):
@@ -37,17 +36,14 @@ with col1:
         if st.button(f"{pts} puntos", key=f"rapido_{pts}_vos"):
             st.session_state.rapido_vos += pts
 
-    st.markdown(f"**Total Vos:** {st.session_state.total_vos}")
-
 with col2:
     st.subheader("Nos")
-    input_nos = st.number_input("Puntos ronda (Nos)", min_value=0, max_value=162, key="input_nos_col")
+    input_nos = st.number_input(min_value=0, max_value=162, key="input_nos_col")
     if input_nos:
         st.session_state.input_nos = input_nos
         st.session_state.input_vos = 162 - input_nos
 
     # Botones rápidos solo afectan a Nos
-    st.markdown("**Puntuaciones rápidas**")
     if st.button("Tercera", key="tercera_nos"):
         st.session_state.rapido_nos += 20
     if st.button("Bolote_Rebolote", key="bolote_nos"):
@@ -55,8 +51,6 @@ with col2:
     for pts in [50, 100, 150, 200]:
         if st.button(f"{pts} puntos", key=f"rapido_{pts}_nos"):
             st.session_state.rapido_nos += pts
-
-    st.markdown(f"**Total Nos:** {st.session_state.total_nos}")
 
 # Botón para pasar a la siguiente ronda
 if st.button("Siguiente ronda"):
@@ -78,7 +72,7 @@ if st.button("Siguiente ronda"):
 
 # Historial en sombreado
 st.markdown("---")
-st.markdown("### Historial de rondas:")
+st.markdown("### Marcador:")
 for i, (vos, nos) in enumerate(st.session_state.historial, start=1):
     st.markdown(f"- **Ronda {i}**: Vos: `{vos}` – Nos: `{nos}` ")
 st.markdown(f"{st.session_state.total_vos} - {st.session_state.total_nos}")
